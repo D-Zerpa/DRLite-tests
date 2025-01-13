cf61c851f154e78c66a8a8316cd5ae4b5ee48315
VonHyde <dariojze@gmail.com>
2025-01-13 18:43:57 +0100

    13/1/25

--- 2/ex2.py
+++ 2/ex2.py
@@ -1,5 +1,20 @@
+list_1 = []
+
 with open('2\input2.txt', 'r') as cal: 
     for line in cal.readlines():
-        id = line.split()
-        
-print(id)
+        list_1.append(line.split())
+
+safe = []
+
+for report in list_1:
+    for level, next in zip(report, report[1:]):
+        if level 
+    
+def safety_check(l):
+    safe = []
+    for level, next in zip(l, l[1:]):
+        if level >= next and (level = next+1 or level = next+2 or level = next+3):
+            
+
+
+print()
